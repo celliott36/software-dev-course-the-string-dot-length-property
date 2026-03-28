b3e79a5e44c49cb45b5c5619cc94b27a5df30bbb
celliott36 <ce90131@gmail.com>
2026-03-28 12:53:03 -0500

    Add files via upload

--- the-string-dot-length-property.js
+++ the-string-dot-length-property.js
@@ -19,12 +19,15 @@ let stringOne = "Coding Bootcamp";
 let stringTwo = "JavaScript";
 let stringThree = "";
 
+
 // Your task: Assign the length of each string to the corresponding variable
-let lengthOne;   // Length of stringOne
-let lengthTwo;   // Length of stringTwo
-let lengthThree; // Length of stringThree
+let lengthOne = stringOne.length;   // Length of stringOne
+let lengthTwo = stringTwo.length;   // Length of stringTwo
+let lengthThree = stringThree.length; // Length of stringThree
 
 // Your code here
-
+console.log(lengthOne);   // Expected output: 15
+console.log(lengthTwo);   // Expected output: 10
+console.log(lengthThree); // Expected output: 0 
 
 
